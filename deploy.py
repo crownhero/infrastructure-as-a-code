6f91f2ed01cdd20d371d83ef43db094c861af670
Tokede Akinkunmi <98185157+crownhero@users.noreply.github.com>
2024-03-30 17:05:44 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -10,6 +10,16 @@ class ThreeTierAppStack(core.Stack):
         # Create VPC
         vpc = Vpc(self, 'MyVpc', max_azs=2)
 
+        security_group = SecurityGroup(self, 'MyInstanceSecurityGroup',
+                                       vpc=vpc,
+                                       allow_all_outbound=True,  
+                                       description='Security group for EC2 instance')
+
+
+        security_group.add_ingress_rule(peer=Peer.any_ipv4(), connection=Port.tcp(80), description='Allow HTTP traffic')
+        security_group.add_ingress_rule(peer=Peer.any_ipv4(), connection=Port.tcp(22), description='Allow SSH connection')
+        
+
         # Create RDS MySQL Database
         rds_database = DatabaseInstance(self, 'MyRDSInstance',
                                          engine=DatabaseInstanceEngine.mysql(version='5.7'),
@@ -21,7 +31,8 @@ class ThreeTierAppStack(core.Stack):
                                     instance_type=InstanceType.of(instance_class='t2.micro', instance_size='micro'),
                                     machine_image=ami-08447c25f2e9dc66c 
                                     vpc=vpc,
-                                    vpc_subnets=SubnetType.PUBLIC)  
+                                    vpc_subnets=SubnetType.PUBLIC
+                                    security_group=security_group)  
         # Create Amplify frontend
         amplify_app = CfnApp(self, 'MyAmplifyApp',
                              name='my-test-amplify-app',
